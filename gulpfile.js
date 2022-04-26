f76369fe68d226ba79601d342074585a51700991
aquastyle <zyzz123y@gmail.com>
2022-04-26 16:28:07 +0300

    addet header and menu

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var cssmin = require('gulp-cssmin');
 
 
 gulp.task('sass', function () {
-    return gulp.src('app/scss/style.scss')
+    return gulp.src('app/scss/**/*.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }))
@@ -79,7 +79,7 @@ gulp.task('js', function () {
 
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+    gulp.watch('app/scss/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
